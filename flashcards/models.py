51911a08f44e6a734722800bad92484749661cee
Anton <garynets.anton@gmail.com>
2024-12-26 20:32:25 +0200

    Add model for english words and add words to db

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class EnglishWords(models.Model):
+    word = models.CharField(max_length=20)
+    translation = models.CharField(max_length=35)
+    level = models.CharField(max_length=2)
+    audio_link = models.CharField(max_length=130)
+    phonetics = models.CharField(max_length=25, blank=True)
+    example = models.CharField(max_length=150, blank=True)
+
+    def __str__(self):
+        return f"{self.word}: {self.translation}"
