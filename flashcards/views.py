bc7c17a0dd3886eca6f5b84a30c99070c9a19613
Anton <garynets.anton@gmail.com>
2024-12-27 20:04:09 +0200

    Added templates, static directories. Created navbar

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import *
 
 def index(request):
-    return HttpResponse("Hello world!")
-    #return render(request, "emphasis/index.html")
+    return render(request, "flashcards/index.html")
